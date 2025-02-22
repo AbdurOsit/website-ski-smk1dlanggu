5d28bd7a34650ac8d086deaafb059201ade1f72e
ABDUR ROSYID <147291816+AbdurOsit@users.noreply.github.com>
2025-02-22 20:13:03 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
